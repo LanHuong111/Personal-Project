6d8be330a64752c87ecf614920620476d858a890
LanHuong111 <lelanhuong2k4@gmail.com>
2024-08-30 08:45:22 +0700

    new project

--- src/member/Login.js
+++ src/member/Login.js
@@ -1,19 +1,43 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Login() {
   const [inputs, setInputs] = useState({
-    email:"",
-    password:""
-  })
-  const handleChangeInput = (e) =>{
-    const {name, value} = e.target
-    console.log(name)
-    console.log(value)
-    setInputs((prev) => ({...prev, [name]:value}))
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState()
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  };
+  console.log(inputs);
+  function handleSubmit(e) {
+    e.preventDefault();
+    let errorsSubmit = {};
+    let flag = true;
+    let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
+    if (!inputs.email) {
+      errorsSubmit.email = " Vui lòng nhập email";
+      flag = false;
+    } else if (!regex.test(inputs.email)) {
+      toast.error("Vui lòng nhập đúng định dạng email");
+      flag = false;
+    } else {
+      console.log("Đã nhập đúng email");
+    }
+    if (!inputs.password) {
+      errorsSubmit.password = " Vui lòng nhập mật khẩu";
+      flag = false;
+    }
+    if (!flag) {
+      setErrors(errorsSubmit);
+    }
   }
-  console.log(inputs)
 
   return (
     <>
@@ -23,10 +47,24 @@ function Login() {
             <div className="col-sm-4 register">
               <div className="signup-form">
                 <h2>Login to your account</h2>
-                <form action="#" className="form-register">
-                  <input name="email" type="text" placeholder="Email Address" onChange={handleChangeInput}/>
+                <form
+                  action="#"
+                  className="form-register"
+                  onSubmit={handleSubmit}
+                >
+                  <input
+                    name="email"
+                    type="text"
+                    placeholder="Email Address"
+                    onChange={handleChangeInput}
+                  />
                   <p>&nbsp;</p>
-                  <input name="password" type="password" placeholder="Password" onChange={handleChangeInput} />
+                  <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChangeInput}
+                  />
                   <p>&nbsp;</p>
                   <span>
                     <Link to="/register">
